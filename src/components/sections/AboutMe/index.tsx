4ffb92b471497163945cc14de5bf9373e78058b0
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-22 11:07:49 -0300

    criando sessao sobre mim estatica

--- src/components/sections/AboutMe/index.tsx
+++ src/components/sections/AboutMe/index.tsx
@@ -1,5 +1,44 @@
-import { SectionAbout } from './styles'
+import {
+  SectionAbout,
+  ContainerAbout,
+  ContentImage,
+  ContentText,
+} from './styles'
 
 export function AboutMe() {
-  return <SectionAbout>oi</SectionAbout>
+  return (
+    <SectionAbout>
+      <ContainerAbout>
+        <ContentImage>
+          <img
+            src="/aboutme.svg"
+            alt="Uma pessoa em pé olhando sua página de perfil ao lado"
+          />
+        </ContentImage>
+        <ContentText>
+          <h2>
+            Sobre <span>mim</span>
+          </h2>
+          <div>
+            <p>
+              Me chamo Guilherme Alvarez Barboza, moro em São Paulo e estou me
+              especializando em <strong>Desenvolvimento Web</strong>.
+            </p>
+
+            <p>
+              Fiz um curso de Desenvolvimento Full Stack na{' '}
+              <strong>Recode Pro</strong> 2020 - 2021, e alguns outros pela
+              udemy, atualmente faço o treinamento Ignite da{' '}
+              <strong>Rocketseat</strong>.
+            </p>
+
+            <p>
+              “Na vida não importa o que você esteja fazendo, faça sempre o seu
+              melhor.” - Ayrton Senna
+            </p>
+          </div>
+        </ContentText>
+      </ContainerAbout>
+    </SectionAbout>
+  )
 }
--- src/components/sections/AboutMe/styles.ts
+++ src/components/sections/AboutMe/styles.ts
@@ -5,3 +5,72 @@ import { Section, Container } from '../../../styles/commonStyles'
 export const SectionAbout = styled(Section)`
   background: ${({ theme }) => theme.black900};
 `
+
+export const ContainerAbout = styled(Container)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+
+  @media (max-width: 868px) {
+    grid-template-columns: 1fr;
+    justify-content: center;
+  }
+`
+
+export const ContentImage = styled.div`
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: min(26rem, 100%);
+  }
+
+  @media (max-width: 868px) {
+    img {
+      width: min(22rem, 100%);
+    }
+  }
+`
+
+export const ContentText = styled.div`
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+
+    span {
+      color: ${({ theme }) => theme.pink500};
+    }
+  }
+
+  div {
+    margin-top: 1.125rem;
+
+    p:not(:first-child) {
+      margin-top: 2.5rem;
+    }
+
+    p {
+      font-size: 1.25rem;
+      strong {
+        color: ${({ theme }) => theme.pink500};
+      }
+    }
+
+    p:last-child {
+      color: ${({ theme }) => theme.white300};
+      font-size: 1rem;
+      font-style: italic;
+    }
+
+    @media (max-width: 868px) {
+      p {
+        font-size: 1.125rem;
+      }
+    }
+  }
+
+  @media (max-width: 868px) {
+    margin-top: 2rem;
+    text-align: center;
+  }
+`

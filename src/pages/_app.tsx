fc9ea4d2e53f44aa36762966766e1a823dcb5eb0
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-28 11:48:11 -0300

    adicionando barra de loading com nextnprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import NextNprogress from 'nextjs-progressbar'
+
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 
-import { GlobalStyles } from '../styles/global'
-
 import { theme } from '../styles/theme'
-
+import { GlobalStyles } from '../styles/global'
 import '../styles/swiperStyles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <NextNprogress
+        color={theme.pink500}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
       <GlobalStyles />
       <Header />
       <Component {...pageProps} />

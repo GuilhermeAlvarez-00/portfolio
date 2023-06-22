f15d28ef4e9880ff73566098b7f3111d4419c538
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-06-22 11:30:23 -0300

    style: adjusting color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,9 @@
 export const theme = {
-  black800: '#11141B',
-  black900: '#040814',
-  white: '#FFFFFF',
-  white200: '#E2E2E2',
-  white300: '#B8B8B8',
-  pink500: '#FF2359',
-}
+  black800: "#11141B",
+  black900: "#040814",
+  white: "#FFFFFF",
+  white200: "#E2E2E2",
+  white300: "#B8B8B8",
+  pink500: "#ff0040",
+};
+
